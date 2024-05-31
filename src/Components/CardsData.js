473453cedfb27c87b5a30a664cdd642047c76e4e
Khuram Shahzad <khuramshahzad54676@gmail.com>
2024-05-31 22:51:07 +0500

    second commit

--- src/Components/CardsData.js
+++ src/Components/CardsData.js
@@ -5,7 +5,7 @@ function CardsData(props) {
     <>
       <div className="pcards">
         <div className="pimage">
-          <img alt="image" src={props.image} />
+          <img alt="" src={props.image} />
         </div>
         <h4 className="font-bold">{props.heading}</h4>
         <p>{props.text}</p>
